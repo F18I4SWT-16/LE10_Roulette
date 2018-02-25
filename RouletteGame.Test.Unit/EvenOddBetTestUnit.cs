ed0a5a3c33293046fb0dcb2973b8553f1f27c2e6
Bjarke Hansen <2takker@gmail.com>
2018-02-25 21:20:59 +0100

    Add files via upload

---
+++ RouletteGame.Test.Unit/EvenOddBetTestUnit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NSubstitute;
+using NUnit.Framework;
+using RouletteGame.Legacy;
+
+namespace RouletteGame.Test.Unit
+{
+    [TestFixture]
+    class EvenOddBetTestUnit
+    {
+        private Bet _uut;
+        private IField _field;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _field = Substitute.For<IField>();
+        }
+
+
+        [TestCase(true, 2u, 4u)]
+        public void WonAmount_CorrectAmountReturned(bool fieldEven, uint amount, uint expected)
+        {
+            // Arrange
+            _uut = new EvenOddBet("Svend", amount, true);
+            _field.Even.Returns(fieldEven);
+
+            // Act & Assert
+            Assert.That(_uut.WonAmount(_field), Is.EqualTo(expected));
+        }
+    }
+}
---
+++ RouletteGame.Test.Unit/FieldBetTestUnit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using NSubstitute;
+using NUnit.Framework;
+using RouletteGame.Legacy;
+
+namespace RouletteGame.Test.Unit
+{
+    [TestFixture]
+    class FieldBetTestUnit
+    {
+        private FieldBet _uut;
+        private IField _field;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _field = Substitute.For<IField>();
+        }
+
+        [TestCase(2u, 2u, 36*2u)]
+        public void WonAmount_CorrectAmountReturned(uint fieldNumber, uint amount, uint expected)
+        {
+            // Arrange
+            _uut = new FieldBet("Svend", amount, 2u);
+            _field.Number.Returns(2u);
+            
+            // Act & Assert
+            Assert.That(_uut.WonAmount(_field), Is.EqualTo(expected));
+        }
+
+    }
+}
